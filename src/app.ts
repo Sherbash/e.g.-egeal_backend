cc0116ccb5c9091e1cad77334a84b60d6cd5d85c
md-sanim-mia <your-vercel-email@example.com>
2025-09-08 23:34:07 +0600

    set cors origin foronted url

--- src/app.ts
+++ src/app.ts
@@ -18,6 +18,7 @@ app.use(
       "http://localhost:3002",
       "https://egealaihub.vercel.app",
       "http://172.252.13.69:3002",
+     "http://206.162.244.131:6002"
     ],
     credentials: true,
   })
